5001aa37df22beb9e46e1d22fd4ed653539263bf
Tristan Menzel <tristanmenzel@gmail.com>
2023-04-10 12:16:56 -0700

    Bug fix for type comparison

--- src/typed-config-factory.ts
+++ src/typed-config-factory.ts
@@ -14,10 +14,21 @@ export type TypedConfig<TConfig extends object, TOpaque> = {
 export type JoinPaths<K, P> = K extends string | number ? (P extends string | number ? `${K}${'' extends P ? '' : '.'}${P}` : never) : never
 export type DefaultOpaqueTypes = Date | Array<any>
 
+/**
+ * Returns true if T exists in TUnionOfTypes.
+ *
+ * Ensures that types are only considered equal if A is assignable to B AND B is assignable to A
+ */
+type ContainsType<T, TUnionOfTypes> = true extends (
+  TUnionOfTypes extends any ? ([T, TUnionOfTypes] extends [TUnionOfTypes, T] ? true : false) : never
+)
+  ? true
+  : false
+
 export type PropertyPaths<T, TOpaque = DefaultOpaqueTypes, D extends number = 10> = [D] extends [never]
   ? never
   : T extends object
-  ? [T, TOpaque] extends [TOpaque, T]
+  ? ContainsType<T, TOpaque> extends true
     ? ''
     : {
         [K in keyof T]-?: K extends string | number
